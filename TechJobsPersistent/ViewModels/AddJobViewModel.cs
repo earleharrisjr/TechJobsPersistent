2ed8adea567598c1e995708a06ab87e4e9ddd1a7
Earl Harris <earleharrisjr@gmail.com>
2022-05-26 11:55:49 -0400

    working on adding a Job still/ restarted

---
+++ TechJobsPersistent/ViewModels/AddJobViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddJobViewModel
+    {
+        [Required(ErrorMessage = "Job name required")]
+        public string Name { get; set; }
+        public int EmployerId { get; set; }
+        public List<SelectListItem> Employers { get; set; }
+    }
+}
